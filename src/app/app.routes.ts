d61ba093fa723da7f48072de3785a9eaded34a22
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-05-29 22:45:57 -0300

    criando toolbar e configurando navegação entre páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
+  },
+  {
+    path: 'consulta',
+    component: ConsultaComponent,
+  },
+];
