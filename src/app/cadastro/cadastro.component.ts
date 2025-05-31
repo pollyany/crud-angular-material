3fde53cbc58c08ee86dcc883960c463820681801
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-05-31 10:20:59 -0300

    Iniciando o formulário de clientes

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrl: './cadastro.component.scss',
 })
-export class CadastroComponent {
-
-}
+export class CadastroComponent {}
