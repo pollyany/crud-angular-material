c4fff106333e4af274a3fd3a33321d97948ed11c
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-05-31 11:00:40 -0300

    Criando o repositorio de clientes para persistir os dados

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -2,13 +2,35 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cadastro/cliente';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  static REPO_CLIENTES = '_CLIENTES';
 
-  constructor() { }
+  constructor() {}
 
-  salvar(cliente: Cliente){
-    console.log(cliente)
+  salvar(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  obterStorage(): Cliente[] {
+    const repositorioClientes = localStorage.getItem(
+      ClienteService.REPO_CLIENTES
+    );
+
+    if (repositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(repositorioClientes);
+
+      return clientes;
+    }
+
+    const clientes: Cliente[] = [];
+
+    localStorage.setItem(
+      ClienteService.REPO_CLIENTES,
+      JSON.stringify(clientes)
+    );
+
+    return clientes;
   }
 }
